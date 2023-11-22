da2ad49d17e2539724655d3bf45c05210d387a05
Pablo Pace <pabloagustinpace@gmail.com>
2023-11-22 15:17:50 +0100

    add methods to linked list

--- algorithm/linked_list.py
+++ algorithm/linked_list.py
@@ -2,11 +2,31 @@ class Node:
     def __init__(self, number, next=None):
         self.number = number
         self.next = next
-    
+
+    def addManyNodes(self, nodes):
+        while self.next != None:
+            self = self.next
+        for node in nodes:
+            nextNode = Node(node)
+            self.next = nextNode
+            self = nextNode
+        return
+        
     def addNodeFinal(self, node):
         while self.next != None:
             self = self.next
             continue
+        self.next = node
+        return
+    
+    def addNodeFirst(self, node):
+        node.next = self
+        return
+    
+    def addAfter(self, number, node):
+        while(self.number != number):
+            continue
+        node.next = self.next
         self.next = node
         return
         
@@ -17,12 +37,15 @@ class Node:
             self = self.next
         print('======================')
         return
+    
+    def getLength(self):
+        c = 0
+        while self != None:
+            c += 1
+            self = self.next
+        return c
 
-head = Node(1) 
+head = Node(1)
+head.addManyNodes([2, 3, 4, 5, 6])
 head.showList()
-child = Node(2)
-head.addNodeFinal(child)
-head.showList()
-child2 = Node(3)
-head.addNodeFinal(child2)
-head.showList()
+print(head.getLength())
